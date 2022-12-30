d04191b7e9feb0c3068b2a19b4d3c29f18a8e381
Aniket Singh <singani@hamiltonstudents.org>
2022-12-29 23:52:38 -0600

    Camera Subsystem
    Camera Subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.TrajectoryUtil;
 import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.PowerDistribution;
@@ -27,9 +28,12 @@ import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.GyroPID;
 import frc.robot.Constants.OIConstants;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.Constants.kGains;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.camera;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
@@ -48,6 +52,9 @@ import java.util.List;
 
 import javax.swing.SwingConstants;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -61,36 +68,47 @@ import javax.swing.SwingConstants;
 public class RobotContainer {
 
   // The robot's subsystems
+
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
-  private final camera m_camera = new camera();
+  PhotonCamera camera = new PhotonCamera("USB Camera 0");
   
 
   // SlewRateLimiter for Joystick Motion Profiling
+
   private final SlewRateLimiter Left = new SlewRateLimiter(3);
   private final SlewRateLimiter Right = new SlewRateLimiter(3);
 
- 
+ //Drive PID Controllers
+
+  PIDController forwardController = new PIDController(kGains.kP, kGains.kI, kGains.kD);
+  PIDController turnController = new PIDController(GyroPID.kP, GyroPID.kI, GyroPID.kD);
   
-  // The driver controllers
+// The driver controllers
+
   XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
   XboxController m_driverController2 = new XboxController(OIConstants.kDriverControllerPort);
 
    // Drive Speeds
-   private double forwardSpeed =  -Left.calculate( m_driverController.getLeftY());
-   private double turnSpeed =  -Right.calculate(m_driverController.getRightY());
 
-   private double leftSpeed =  -Left.calculate( m_driverController.getLeftY());
-   private double rightSpeed =  -Right.calculate(m_driverController.getRightY());
+   private double forwardSpeed;
+   private double turnSpeed;
+
+  //  private double leftSpeed =  -Left.calculate( m_driverController.getLeftY());
+  //  private double rightSpeed =  -Right.calculate(m_driverController.getRightY());
     
   public RobotContainer() {
 
     //Toggle Booleans
+
     boolean toggleFastMode = true;
     boolean toggleSlowMode = true;
+    
 
     //Joystick Buttons
+
     JoystickButton startButton = new JoystickButton(m_driverController, Button.kStart.value);
     JoystickButton backButton = new JoystickButton(m_driverController, Button.kBack.value);
+    
 
 
    //Button Bindings
@@ -105,22 +123,49 @@ public class RobotContainer {
         (new toggleSlowMode(m_robotDrive));
     }
 
+    
+    if (m_driverController.getAButton()) {
+      
+      var result = camera.getLatestResult();
 
+      if (result.hasTargets()) {
+        
+        double range = PhotonUtils.calculateDistanceToTargetMeters(
+          VisionConstants.CAMERA_HEIGHT_METERS, 
+          VisionConstants.TARGET_HEIGHT_METERS, 
+          VisionConstants.CAMERA_PITCH_RADIANS,  
+          Units.degreesToRadians(result.getBestTarget().getPitch()));
 
+          forwardSpeed = -forwardController.calculate(range, VisionConstants.GOAL_RANGE_METERS);
+
+          turnSpeed = -turnController.calculate(result.getBestTarget().getYaw(), 0);
+
+      } else {
+          forwardSpeed = 0;
+          turnSpeed = 0;
+          
+      }  
+
+        
+      
+  }
+
+  forwardSpeed = -Left.calculate( m_driverController.getLeftY());
+  turnSpeed = -Right.calculate(m_driverController.getRightX());
 
    //Drive Commands
 
     m_robotDrive.setDefaultCommand(
-      new RunCommand(() ->
-          m_robotDrive.tankDrive(
-            leftSpeed,
-             rightSpeed),
-              m_robotDrive)
+      // new RunCommand(() ->
+      //     m_robotDrive.tankDrive(
+      //       leftSpeed,
+      //        rightSpeed),
+      //         m_robotDrive)
       
-      // new ArcadeDriveCommand(
-      //   m_robotDrive,
-      //   () -> forwardSpeed,
-      //   () -> turnSpeed)
+      new ArcadeDriveCommand(
+        m_robotDrive,
+        () -> forwardSpeed,
+        () -> turnSpeed)
 );
       
 
