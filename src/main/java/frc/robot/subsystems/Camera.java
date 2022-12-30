570fa327287f9ff65299e9b9cc3e7c3c8e62d4dd
Aniket Singh <singani@hamiltonstudents.org>
2022-12-30 11:00:10 -0600

    Camera SmartDashboard
    Camera SmartDashboard

---
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Camera {
+
+    PhotonCamera camera = new PhotonCamera("USB Camera 0");
+    PhotonPipelineResult result = camera.getLatestResult();
+    PhotonTrackedTarget target = result.getBestTarget();
+    boolean hasTargets = result.hasTargets();
+    
+public void camera(){
+
+SmartDashboard.putBoolean("Target Spotted", hasTargets);
+SmartDashboard.putNumber("Target Yaw", target.getYaw());
+SmartDashboard.putNumber("Target Pitch", target.getPitch());
+SmartDashboard.putNumber("Target Skew", target.getSkew());
+SmartDashboard.putNumber("Target Area", target.getArea());   
+
+
+}
+
+    public PhotonPipelineResult getLatestResult(){
+
+    return camera.getLatestResult();
+
+    }
+    
+    
+}
