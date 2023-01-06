36ff5e391ec4636b90451e4aa985a643896c9999
Aniket Singh <singani@hamiltonstudents.org>
2023-01-05 21:17:10 -0600

    Chooser Update
    Chooser Update

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -108,6 +108,9 @@ public class RobotContainer {
 
     JoystickButton startButton = new JoystickButton(m_driverController, Button.kStart.value);
     JoystickButton backButton = new JoystickButton(m_driverController, Button.kBack.value);
+
+
+    // String trajectoryTestJSON = "paths/Test1.wpilib.json";
     
 
    //Button Bindings
@@ -171,43 +174,34 @@ public class RobotContainer {
 //Choose Which Drive Based on what is chosen by Drivers
     
   }
-  public Command getAutonomousCommand() {
+  public Command getAutonomousCommand(Trajectory trajectory) {
     // Voltage Restricition to Prevent Motor from Blowing itself
   
-    var autoVoltageConstraint =
-    new DifferentialDriveVoltageConstraint(
-        new SimpleMotorFeedforward(
-            DriveConstants.ksVolts,
-            DriveConstants.kvVoltSecondsPerMeter,
-            DriveConstants.kaVoltSecondsSquaredPerMeter),
-        DriveConstants.kDriveKinematics,
-        10);
+   
+
+    // var autoVoltageConstraint =
+    // new DifferentialDriveVoltageConstraint(
+    //     new SimpleMotorFeedforward(
+    //         DriveConstants.ksVolts,
+    //         DriveConstants.kvVoltSecondsPerMeter,
+    //         DriveConstants.kaVoltSecondsSquaredPerMeter),
+    //     DriveConstants.kDriveKinematics,
+    //     10);
   
     // Create config for trajectory
-    TrajectoryConfig config =
-    new TrajectoryConfig(
-            AutoConstants.kMaxSpeedMetersPerSecond,
-            AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-        // Add kinematics to ensure max speed is actually obeyed
-        .setKinematics(DriveConstants.kDriveKinematics)
-        // Apply the voltage constraint
-        .addConstraint(autoVoltageConstraint);
+    // TrajectoryConfig config =
+    // new TrajectoryConfig(
+    //         AutoConstants.kMaxSpeedMetersPerSecond,
+    //         AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+    //     // Add kinematics to ensure max speed is actually obeyed
+    //     .setKinematics(DriveConstants.kDriveKinematics)
+    //     // Apply the voltage constraint
+    //     .addConstraint(autoVoltageConstraint);
   
-    Trajectory exampleTrajectory =
-          TrajectoryGenerator.generateTrajectory(
-              // Start at the origin facing the +X direction
-              new Pose2d(0, 0, new Rotation2d(0)),
-              // Pass through these two interior waypoints, making an 's' curve path
-              List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-              // End 3 meters straight ahead of where we started, facing forward
-              new Pose2d(3, 0, new Rotation2d(0)),
-              // Pass config
-              config);
-  
-  
+
     RamseteCommand ramseteCommand =
           new RamseteCommand(
-              exampleTrajectory,
+              trajectory,
               m_robotDrive::getPose,
               new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
               new SimpleMotorFeedforward(
@@ -224,7 +218,7 @@ public class RobotContainer {
   
   
               // Reset odometry to the starting pose of the trajectory.
-      m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
+      m_robotDrive.resetOdometry(trajectory.getInitialPose());
   
   
       return ramseteCommand.andThen(() -> m_robotDrive.tankDriveVolts(0, 0));
