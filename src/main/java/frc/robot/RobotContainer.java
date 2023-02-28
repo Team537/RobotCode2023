7eca395254ad8f16531d617555d4147348afde9b
Aniket Singh <singani@hamiltonstudents.org>
2023-02-28 15:28:58 -0600

    Commented Out

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -190,42 +190,42 @@ public class RobotContainer {
 
 // NON LED COMMANDS
 
-    // dPadLeftButton.onTrue(new StartEndCommand(m_ArmInOut::armMidGoal,m_ArmInOut::armLowGoal,m_ArmInOut));
-    // dPadRightButton.onTrue(new StartEndCommand(m_ArmInOut::armLowGoal,m_ArmInOut::armMidGoal,m_ArmInOut));
-    // leftBumper.onTrue(new StartEndCommand(m_ArmInOut::armHighGoal,m_ArmInOut::armMidGoal,m_ArmInOut));
+    dPadLeftButton.onTrue(new StartEndCommand(m_ArmInOut::armMidGoal,m_ArmInOut::armLowGoal,m_ArmInOut));
+    dPadRightButton.onTrue(new StartEndCommand(m_ArmInOut::armLowGoal,m_ArmInOut::armMidGoal,m_ArmInOut));
+    leftBumper.onTrue(new StartEndCommand(m_ArmInOut::armHighGoal,m_ArmInOut::armMidGoal,m_ArmInOut));
 
 
     // // dPadUpButton.onTrue(new StartEndCommand(m_ArmInOut::armIncrementUp, m_ArmInOut::armIncrementDown, m_ArmInOut));
     // // dPadDownButton.onTrue(new StartEndCommand(m_ArmInOut::armIncrementDown, m_ArmInOut::armIncrementUp, m_ArmInOut));
     // /*^^ for incrementing the position of the arm in-out */
 
-    // yButton.onTrue(new StartEndCommand(m_ArmPivot::ArmPositionLowGoal,m_ArmPivot::ArmPositionHighGoal,m_ArmPivot));
-    // xButton.onTrue(new StartEndCommand(m_ArmPivot::ArmPositionHighGoal,m_ArmPivot::ArmPositionLowGoal,m_ArmPivot));
-    // backButton.onTrue(new StartEndCommand(m_ArmPivot::ArmPositionMidGoal,m_ArmPivot::ArmPositionLowGoal,m_ArmPivot));
+    yButton.onTrue(new StartEndCommand(m_ArmPivot::ArmPositionLowGoal,m_ArmPivot::ArmPositionHighGoal,m_ArmPivot));
+    xButton.onTrue(new StartEndCommand(m_ArmPivot::ArmPositionHighGoal,m_ArmPivot::ArmPositionLowGoal,m_ArmPivot));
+    backButton.onTrue(new StartEndCommand(m_ArmPivot::ArmPositionMidGoal,m_ArmPivot::ArmPositionLowGoal,m_ArmPivot));
 
-    // dPadDownButton.onTrue(new StartEndCommand(m_Wrist::WristPositionLowGoal,m_Wrist::WristPositionHighGoal,m_Wrist));
-    // dPadUpButton.onTrue(new StartEndCommand(m_Wrist::WristPositionHighGoal,m_Wrist::WristPositionMidGoal,m_Wrist));
-    // leftBumper.onTrue(new StartEndCommand(m_Wrist::WristPositionMidGoal,m_Wrist::WristPositionHighGoal,m_Wrist));
-    // rightBumper.onTrue(new StartEndCommand(m_Wrist::WristPositionZero,m_Wrist::WristPositionHighGoal,m_Wrist));
+    dPadDownButton.onTrue(new StartEndCommand(m_Wrist::WristPositionLowGoal,m_Wrist::WristPositionHighGoal,m_Wrist));
+    dPadUpButton.onTrue(new StartEndCommand(m_Wrist::WristPositionHighGoal,m_Wrist::WristPositionMidGoal,m_Wrist));
+    leftBumper.onTrue(new StartEndCommand(m_Wrist::WristPositionMidGoal,m_Wrist::WristPositionHighGoal,m_Wrist));
+    rightBumper.onTrue(new StartEndCommand(m_Wrist::WristPositionZero,m_Wrist::WristPositionHighGoal,m_Wrist));
 
-    //  aButton.toggleOnTrue(new StartEndCommand(m_Gripper::GripperIn,m_Gripper::GripperStop,m_Gripper));
-    // bButton.toggleOnTrue(new StartEndCommand(m_Gripper::GripperOut,m_Gripper::GripperStop,m_Gripper));
+     aButton.toggleOnTrue(new StartEndCommand(m_Gripper::GripperIn,m_Gripper::GripperStop,m_Gripper));
+    bButton.toggleOnTrue(new StartEndCommand(m_Gripper::GripperOut,m_Gripper::GripperStop,m_Gripper));
 
 
     //  LED COMMANDS
-    yButton.onTrue(high_goal);
-    xButton.onTrue(mid_goal);
+    // yButton.onTrue(high_goal);
+    // xButton.onTrue(mid_goal);
 
    
-    aButton.onTrue(low_goal);
-    bButton.onTrue(shelf);
+    // aButton.onTrue(low_goal);
+    // bButton.onTrue(shelf);
 
 
-     leftBumper.toggleOnTrue(gripperIn);
-     rightBumper.toggleOnTrue(gripperOut);
+    //  leftBumper.toggleOnTrue(gripperIn);
+    //  rightBumper.toggleOnTrue(gripperOut);
 
-     dPadLeftButton.toggleOnTrue(signalCone);
-     dPadRightButton.toggleOnTrue(signalCube);
+    //  dPadLeftButton.toggleOnTrue(signalCone);
+    //  dPadRightButton.toggleOnTrue(signalCube);
 
     
      final ChaseTagCommand chaseTagCommand = 
