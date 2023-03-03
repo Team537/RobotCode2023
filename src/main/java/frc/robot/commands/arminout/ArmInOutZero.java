0d0cb8469ae69ccc2e74685f3008c2bf4cfafb20
Aniket Singh <singani@hamiltonstudents.org>
2023-03-03 17:46:56 -0600

    merge fixes

--- src/main/java/frc/robot/commands/arminout/ArmInOutZero.java
+++ src/main/java/frc/robot/commands/arminout/ArmInOutZero.java
@@ -2,20 +2,21 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.armpivot;
+package frc.robot.commands.arminout;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.manipulator.ArmPivot;
+import frc.robot.subsystems.manipulator.ArmInOut;
 
-public class ArmPivotZero extends CommandBase {
 
-  private ArmPivot m_ArmPivot;
+public class ArmInOutZero extends CommandBase {
+
+  private ArmInOut m_ArmInOut;
   /** Creates a new ArmPivotLowGoal. */
-  public ArmPivotZero(ArmPivot m_ArmPivot) {
+  public ArmInOutZero(ArmInOut m_ArmInOut) {
 
-    this.m_ArmPivot = m_ArmPivot;
+    this.m_ArmInOut = m_ArmInOut;
 
-    addRequirements(m_ArmPivot);
+    addRequirements(m_ArmInOut);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
@@ -27,7 +28,7 @@ public class ArmPivotZero extends CommandBase {
   @Override
   public void execute() {
 
-    m_ArmPivot.ArmPositionZero();
+    m_ArmInOut.armZero();
   }
 
   // Called once the command ends or is interrupted.
