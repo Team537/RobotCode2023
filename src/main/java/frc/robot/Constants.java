d3effb380d9cb975f446244457fc898fd6e83cd2
MaxwellMcLaughlin <127265918+MaxwellMcLaughlin@users.noreply.github.com>
2023-03-09 13:04:05 -0600

    Comment update 2
    finished commenting Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,13 +33,15 @@ import frc.robot.utils.ModuleMap;
  * constants are needed, to reduce verbosity.
  */
 
- // Makes the main class of the file (everything in the file goes in this)(works like a file itself)
+// Makes the main class of the file (everything in the file goes in this)(works
+// like a file itself)
 public class Constants {
 
   // Spark Max Constants
   public static final class SparkPIDFConstants {
 
-    // This is the default PIDF for spark maxes. Perportional, Integral, Derivetive, Izone and Feed Forward. 
+    // This is the default PIDF for spark maxes. Perportional, Integral, Derivetive,
+    // Izone and Feed Forward.
     public static final double kP = 0;
     public static final double kI = 0;
     public static final double kD = 0;
@@ -50,13 +52,14 @@ public class Constants {
     public static final double kMaxOutput = 1;
     public static final double kMinOutput = -1;
 
-    //This is the max cruise velocity in RPM. This is the speed it travels at when going to the set position
+    // This is the max cruise velocity in RPM. This is the speed it travels at when
+    // going to the set position
     public static final double kMaxV = 3000;
     public static final double kMinV = 0;
 
-    //This is the max acceleration when accelerating to the cruise velocity. In RPM
+    // This is the max acceleration when accelerating to the cruise velocity. In RPM
     public static final double kMaxA = 3000;
-    //specific accel constant for the pivot
+    // specific accel constant for the pivot
     public static final double kMaxAccelArmPivot = 2000;
 
     // allowed closed loop error
@@ -68,7 +71,7 @@ public class Constants {
   public static final class DriveConstants {
 
     public static final double kTrackwidthMeters = 0.415;
-    public static final double kWheelBase =  0.415;
+    public static final double kWheelBase = 0.415;
     public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(
         kTrackwidthMeters);
 
@@ -77,13 +80,12 @@ public class Constants {
     public static final double kEncoderDistancePerPulse =
         // Assumes the encoders are directly mounted on the wheel shafts
         (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
-        
+
     public static final double ksVolts = 0.305;
     public static final double kvVoltSecondsPerMeter = 2.29;
     public static final double kaVoltSecondsSquaredPerMeter = 0.0131;
 
     public static final double kPDriveVel = 8.5;
-
 
     // Gyro Command Constants
     public static final double kTurnP = 8.5;
@@ -94,50 +96,50 @@ public class Constants {
 
   }
 
-  public static final class GripperConstants{
+  public static final class GripperConstants {
 
     // These are the ids for each gripper motor
-    // TODO: Temporary variables ( find a permanent solution  ex. interface)
+    // TODO: set temporary variables on an interface/dashboard
     public static final int kGripper = 15;
-    public static final int kGripper2 = 17; 
+    public static final int kGripper2 = 17;
 
   }
-  
+
   // Sets all variables/constants for arm movement
   public static final class ArmInOutConstants {
 
     // Motor id for inout
-    // TODO: Temporary variables ( find a permanent solution  ex. interface)
     public static final int kArmInOut = 14;
 
-    //These are the set positions for the differnt scoring heights on the arm inout motor.
+    // These are the set positions for the differnt scoring heights on the arm inout
+    // motor.
     // current limits are (+-)(it is +- because the string can get flipped) 300 to 0
 
-    // TODO: Temporary variables ( find a permanent solution  ex. interface)
     public static final double kArmInOutPositionHighGoal = 100; // for "shooting" cones, it was 100
     // the value for normal high goal is 225
 
-    // TODO: Temporary variables ( find a permanent solution  ex. interface)
     public static final double kArmInOutPositionMidGoal = 50;
     public static final double kArmInOutPositionShelfHumanPL = 50;
     public static final double kArmInOutPositionShelfHigh = 50;
     public static final double kArmInOutPositionGround = 0.0;
-    
-    //used for setting the robot to zero (and accounts for the string rolling up different)
-    // TODO: Temporary variables ( find a permanent solution  ex. interface)
+
+    // used for setting the robot to zero (and accounts for the string rolling up
+    // different)
     public static final double kArmInOutPositionZero = -1;
 
-
-    //positions used to increment inout positions
-    /*public static final double kIncrementUp = -10;
-    public static final double kIncrementDown = 10;*/
+    // positions used to increment inout positions
+    /*
+     * public static final double kIncrementUp = -10;
+     * public static final double kIncrementDown = 10;
+     */
   }
 
+  // Sets arm variables/constants
   public static final class ArmPivotConstants {
-    // the id for the arm pivot constant. 
+    // The id for the arm pivot constant.
     public static final int kArmPivot1 = 19;
-   
-    //these are the setpositions for the armpivot. current limits are 0 to 58
+
+    // These are the setpositions for the armpivot. current limits are 0 to 58
     public static final double kArmPivotPositionHighGoal = 17;
     public static final double kArmPivotPositionShelfHumanPL = 15;
     public static final double kArmPivotPositionMidGoal = 20;
@@ -146,41 +148,45 @@ public class Constants {
     public static final double kArmPivotPositionZero = 0.0;
     public static final double kArmPivotPositionGround = 58.0;
 
-    // this is the position for moving tht arm down a bit when scoring mid.  via tylers prefrences
+    // This is the position for moving tht arm down a bit when scoring mid. via Main
+    // Driver's prefrences
     public static final double kArmPivotPositionMidDown = 23;
 
   }
 
   public static final class WristConstants {
-    //motor id for wrist
+    // Motor id for wrist
     public static final int kWrist = 11;
 
-    // set positions for the wrist. known limits are 0 to -17(could be farther)
+    // Set positions for the wrist. known limits are 0 to -17(could be farther)
     public static final double kWristPositionHighGoal = -11;
     public static final double kWristPositionShelfHumanPL = -17;
-    public static final double kWristPositionMidGoal = -13; 
+    public static final double kWristPositionMidGoal = -13;
     public static final double kWristPositionShelfHigh = -10;
 
+    // Wrist zero and ground positions
     public static final double kWristPositionZero = 0;
     public static final double kWristPositionGround = -6;
 
-    //when carrying cones and cubes on the ground, they currently hit
-    //the ground, need to add something to bring the wrist up 1 rotation
+    // when carrying cones and cubes on the ground, they currently hit
+    // the ground, need to add something to bring the wrist up 1 rotation
     public static final double kWristPositionManualUp = -3;
     public static final double kWristPositionManualDown = -15;
 
-
   }
 
+  // Output/Input port constants
   public static final class OIConstants {
 
-    //these are the ports for each controller. the controller ports can be switched in the driver station
+    // these are the ports for each controller. the controller ports can be switched
+    // in the driver station
     public static final int kDriverControllerPort = 0;
     public static final int kDriverControllerPort1 = 1;
   }
 
+  // Holds methods for metric & imperial conversions
   public final static class Conversions {
-    
+
     public static double metersToFeet(double meters) {
       return meters * 3.28084;
     }
@@ -211,62 +217,66 @@ public class Constants {
 
   }
 
+  // Sets variables for camera and field positions
   public static class VisionConstants {
     public static final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);
     public static final double TARGET_HEIGHT_METERS = Units.feetToMeters(5);
     public static final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
     public static final double GOAL_RANGE_METERS = Units.feetToMeters(3);
-    public static final Transform3d CAMERA_TO_ROBOT =
-        new Transform3d(new Translation3d(0.0, -0.1375, -.90), new Rotation3d(0.0, 0.0, -0.10));
-        public static final double FIELD_LENGTH_METERS = 16.54175;
+    public static final Transform3d CAMERA_TO_ROBOT = new Transform3d(new Translation3d(0.0, -0.1375, -.90),
+        new Rotation3d(0.0, 0.0, -0.10));
+    public static final double FIELD_LENGTH_METERS = 16.54175;
     public static final double FIELD_WIDTH_METERS = 8.0137;
-  } 
+  }
 
+  // Limelight pipelines
   public static class limelight {
     public static final int kCamera = 0;
     public static final int kLimelight = 1;
     public static final int kAprilTagRed = 2;
     public static final int kAprilTagBlue = 3;
-
-
   }
 
+  // LED RGB/addressable constants
   public static class LEDConstants {
-  public static final int length = 59;
-  public static final int centerLed = 29;
-  public static final int halfLength = (int) Math.ceil(length / 2.0);
-  public static final int batteryStartIndex = 72;
-  public static final int batteryEndIndex = 118;
-  public static final double strobeDuration = 0.2; 
-  public static final double rainbowFastFullLength = 40.0; 
-  public static final double rainbowFastDuration = 0.25; 
-  public static final double rainbowSlowFullLength = 80.0; 
-  public static final double rainbowSlowDuration = 4.0; 
-  public static final double breathDuration = 2.0; 
-  public static final double waveExponent = 0.4; 
-  public static final double waveFastFullLength = 40.0; 
-  public static final double waveFastDuration = 0.25; 
-  public static final double waveAllianceFullLength = 15.0; 
-  public static final double waveAllianceDuration = 2.0; 
-  public static final double waveSlowFullLength = 40.0; 
-  public static final double waveSlowDuration = 3.0; 
+    public static final int length = 59;
+    public static final int centerLed = 29;
+    public static final int halfLength = (int) Math.ceil(length / 2.0);
+    public static final int batteryStartIndex = 72;
+    public static final int batteryEndIndex = 118;
+    public static final double strobeDuration = 0.2;
+    public static final double rainbowFastFullLength = 40.0;
+    public static final double rainbowFastDuration = 0.25;
+    public static final double rainbowSlowFullLength = 80.0;
+    public static final double rainbowSlowDuration = 4.0;
+    public static final double breathDuration = 2.0;
+    public static final double waveExponent = 0.4;
+    public static final double waveFastFullLength = 40.0;
+    public static final double waveFastDuration = 0.25;
+    public static final double waveAllianceFullLength = 15.0;
+    public static final double waveAllianceDuration = 2.0;
+    public static final double waveSlowFullLength = 40.0;
+    public static final double waveSlowDuration = 3.0;
   }
 
+  // Sets Contansts used in swerve drive
   public static class SwerveConstants {
+
+    // Wheel physics
     public static final double kWheelRadius = 1.75;
     public static final int kEncoderResolution = 4096;
-    public static final double  kDriveMotorGearRatio = 7.13; 
+    public static final double kDriveMotorGearRatio = 7.13;
     public static final double kTurningMotorGearRatio = 15.428;
     public static final double kMaxSpeed = 15.0;
-    public static final double kModuleMaxAngularVelocity = Math.PI*4;
-    public static final double kModuleMaxAngularAcceleration = 3 * Math.PI; 
+    public static final double kModuleMaxAngularVelocity = Math.PI * 4;
+    public static final double kModuleMaxAngularAcceleration = 3 * Math.PI;
 
     public static final double kPModuleTurningController = 0;
     public static final double kPModuleDriverController = 0;
 
     public static final double kSTurn = 0.1;
     public static final double kVTurn = 0.2;
-    
+
     public static final double kSDrive = 0.587;
     public static final double kVDrive = 2.3;
     public static final double kADrive = 0.517;
@@ -275,18 +285,15 @@ public class Constants {
     public static final double kvDriveVoltSecondsSquaredPerMeter = 0.01;
     public static final double kaDriveVoltSecondsSquaredPerMeter = .05;
 
-
-    public static final double kTurningEncoderDistancePerPulse =
-    
-    360 / (DriveConstants.kEncoderCPR* kTurningMotorGearRatio);
+    public static final double kTurningEncoderDistancePerPulse = 
+     360 / (DriveConstants.kEncoderCPR * kTurningMotorGearRatio);
 
     public static final double kDriveEncoderDistancePerPulse =
-    (2*kWheelRadius * Math.PI) / (DriveConstants.kEncoderCPR * kDriveMotorGearRatio);
+     (2 * kWheelRadius * Math.PI) / (DriveConstants.kEncoderCPR * kDriveMotorGearRatio);
 
     public static final double kMaxSpeedMetersPerSecond = 8;
     public static final double kMaxRotationRadiansPerSecond = Math.PI * 4;
     public static final double kMaxRotationRadiansPerSecondSquared = Math.PI * 1;
-    
 
     public static final double kSlowRotationRadiansPerSecond = Math.PI * 1;
 
@@ -298,13 +305,13 @@ public class Constants {
     public static final int kBackLeftTurn = 5;
     public static final int kBackRightDrive = 6;
     public static final int kBackRightTurn = 7;
-    
+
     public static final int kFrontLeftSRXMagCoder = 0;
     public static final int kFrontRightSRXMagCoder = 1;
     public static final int kBackLeftSRXMagCoder = 2;
     public static final int kBackRightSRXMagCoder = 3;
 
-    public static final double kFrontLeftSRXMagCoderOffset = 320;//175;
+    public static final double kFrontLeftSRXMagCoderOffset = 320;// 175;
     public static final double kFrontRightSRXMagCoderOffset = 0;
     public static final double kBackLeftSRXMagCoderOffset = 0.;
     public static final double kBackRightSRXMagCoderOffset = 0.;
@@ -319,41 +326,40 @@ public class Constants {
     public static final double kI_Rot = 0;
     public static final double kD_Rot = 0.01;
 
-   public static final int kPigeonID = 10;
+    public static final int kPigeonID = 10;
 
-    public static final TrapezoidProfile.Constraints kRotControllerConstraints = new TrapezoidProfile.Constraints(
-        kMaxRotationRadiansPerSecond, kMaxRotationRadiansPerSecondSquared);
+    // PID controller constraints
+    public static final TrapezoidProfile.Constraints kRotControllerConstraints = 
+      new TrapezoidProfile.Constraints(kMaxRotationRadiansPerSecond, kMaxRotationRadiansPerSecondSquared);
 
+    // Swerve Module Map
+    public static final Map<ModulePosition, Translation2d> kModuleTranslations = Map.of(
+        ModulePosition.FRONT_LEFT,
+        new Translation2d(-DriveConstants.kWheelBase / 2, DriveConstants.kTrackwidthMeters / 2),
+        ModulePosition.FRONT_RIGHT,
+        new Translation2d(-DriveConstants.kWheelBase / 2, -DriveConstants.kTrackwidthMeters / 2),
+        ModulePosition.BACK_LEFT,
+        new Translation2d(DriveConstants.kWheelBase / 2, -DriveConstants.kTrackwidthMeters / 2),
+        ModulePosition.BACK_RIGHT,
+        new Translation2d(DriveConstants.kWheelBase / 2, DriveConstants.kTrackwidthMeters / 2)
+    );
 
-        public static final Map<ModulePosition, Translation2d> kModuleTranslations =
-        Map.of(
-            ModulePosition.FRONT_LEFT, new Translation2d(-DriveConstants.kWheelBase / 2, DriveConstants.kTrackwidthMeters / 2),
-            ModulePosition.FRONT_RIGHT, new Translation2d(-DriveConstants.kWheelBase / 2, -DriveConstants.kTrackwidthMeters / 2),
-            ModulePosition.BACK_LEFT, new Translation2d(DriveConstants.kWheelBase / 2, -DriveConstants.kTrackwidthMeters / 2),
-            ModulePosition.BACK_RIGHT, new Translation2d(DriveConstants.kWheelBase / 2, DriveConstants.kTrackwidthMeters / 2));
+    // Based on the map, sets physics to the Swerve modules
+    public static final SwerveDriveKinematics kDriveKinematics = 
+      new SwerveDriveKinematics(ModuleMap.orderedValues(kModuleTranslations, new Translation2d[0]));
 
-    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics( 
-      ModuleMap.orderedValues(kModuleTranslations, new Translation2d[0]));
-           
-
-        public enum ModulePosition {
-          FRONT_LEFT,
-          FRONT_RIGHT,
-          BACK_LEFT,
-          BACK_RIGHT
-        }
+    // Swerve Module Position enum
+    public enum ModulePosition {
+      FRONT_LEFT,
+      FRONT_RIGHT,
+      BACK_LEFT,
+      BACK_RIGHT
+    }
   }
   // public class AutoConstants{
 
-  //   public HashMap<String, Command> autoMap = new HashMap<>();
+  // public HashMap<String, Command> autoMap = new HashMap<>();
 
-    
-    
-    
-  
   // }
-  
+
 }
-
-
-
