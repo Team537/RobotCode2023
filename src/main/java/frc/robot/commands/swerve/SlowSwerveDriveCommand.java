7627dff04144b022e28d44f8bbec0210c6b9dc8a
MaxwellMcLaughlin <127265918+MaxwellMcLaughlin@users.noreply.github.com>
2023-03-16 20:24:04 -0500

    Update SlowSwerveDriveCommand.java

--- src/main/java/frc/robot/commands/swerve/SlowSwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/swerve/SlowSwerveDriveCommand.java
@@ -18,7 +18,7 @@ import java.util.function.DoubleSupplier;
  * An example command that uses an example subsystem.
  */
 public class SlowSwerveDriveCommand extends CommandBase {
-  
+
   private final DriveSubsystem m_drive;
   private final DoubleSupplier m_driveInput, m_strafeInput, m_rotationInput;
   private final boolean m_isFieldRelative;
@@ -29,7 +29,8 @@ public class SlowSwerveDriveCommand extends CommandBase {
    *
    * 
    */
-  public SlowSwerveDriveCommand(DriveSubsystem m_drive, DoubleSupplier driveInput, DoubleSupplier strafeInput, DoubleSupplier rotationInput, boolean isFieldRelative, LED m_LED) {
+  public SlowSwerveDriveCommand(DriveSubsystem m_drive, DoubleSupplier driveInput, DoubleSupplier strafeInput,
+      DoubleSupplier rotationInput, boolean isFieldRelative, LED m_LED) {
     this.m_drive = m_drive;
     m_driveInput = driveInput;
     m_strafeInput = strafeInput;
@@ -43,39 +44,40 @@ public class SlowSwerveDriveCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    System.out.print( " \n Drive Started");
+    System.out.print(" \n Drive Started");
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-                                                      // 0.05 are the joystick deadbands. if the joystick is less than that value, is makes it equal to 0 
+    // 0.05 are the joystick deadbands. if the joystick is less than that value, is
+    // makes it equal to 0
     double drive = Math.abs(m_driveInput.getAsDouble()) > 0.05 ? m_driveInput.getAsDouble() : 0;
     double strafe = Math.abs(m_strafeInput.getAsDouble()) > 0.05 ? m_strafeInput.getAsDouble() : 0;
     double rotation = Math.abs(m_rotationInput.getAsDouble()) > 0.05 ? m_rotationInput.getAsDouble() : 0;
 
-
-    m_drive.slowDrive(drive, strafe, rotation, m_isFieldRelative, true);    // Forward/Back drive, Left/Right Strafe, Left/Right Turn
-    if(Math.abs(m_drive.getVelocity()) > 0 && m_drive.driveState.equals("Drive") && DriverStation.isTeleop()) {
+    m_drive.slowDrive(drive, strafe, rotation, m_isFieldRelative, true); // Forward/Back drive, Left/Right Strafe,
+                                                                         // Left/Right Turn
+    if (Math.abs(m_drive.getVelocity()) > 0 && m_drive.driveState.equals("Drive") && DriverStation.isTeleop()) {
       m_LED.setDriving(true);
       m_LED.setBoostDriving(false);
-      
-    } else{
+
+    } else {
       m_LED.setDriving(false);
 
     }
 
-    if(m_triggerInput.getAsDouble() > 0 ){
+    if (m_triggerInput.getAsDouble() > 0) {
       m_LED.setBoostDriving(true);
       m_LED.setDriving(false);
     } else {
       m_LED.setBoostDriving(false);
     }
 
-    //detects pitch and roll from the start position
-    if(Math.abs(m_drive.getPitch()) > 75 || Math.abs(m_drive.getRoll()) > 75) {
+    // detects pitch and roll from the start position
+    if (Math.abs(m_drive.getPitch()) > 75 || Math.abs(m_drive.getRoll()) > 75) {
       m_LED.setFallen(true);
-    } else{
+    } else {
       m_LED.setFallen(false);
     }
   }
