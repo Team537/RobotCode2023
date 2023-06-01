8e3cf65df4dc3725c417d8e977680616d8c56c9f
LWiscool7 <lwambach21@gmail.com>
2023-06-01 18:33:16 -0500

    updates

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RobotCode2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 257;
-  public static final String GIT_SHA = "595ca0041e6fea69025ab38f2aa1fb206c61df38";
-  public static final String GIT_DATE = "2023-04-13 19:53:01 CDT";
+  public static final int GIT_REVISION = 258;
+  public static final String GIT_SHA = "c4970eb1e866e2040ac243a07011e168e035bab5";
+  public static final String GIT_DATE = "2023-04-13 20:44:58 CDT";
   public static final String GIT_BRANCH = "smart-swerbe";
-  public static final String BUILD_DATE = "2023-04-13 20:42:33 CDT";
-  public static final long BUILD_UNIX_TIME = 1681436553484L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2023-05-25 19:50:38 CDT";
+  public static final long BUILD_UNIX_TIME = 1685062238162L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
